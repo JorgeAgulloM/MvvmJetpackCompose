1ab880f774a25e150a416580256acfcf8f37d13c
Jorge1 <agullojorge@gmail.com>
2024-01-15 22:05:57 +0100

    Implemented appModule

---
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/core/AppModule.kt
@@ -0,0 +1,36 @@
+package com.softyorch.mvvmjetpackcompose.core
+
+import android.content.Context
+import androidx.room.Room
+import com.softyorch.mvvmjetpackcompose.data.AppDatabase
+import com.softyorch.mvvmjetpackcompose.data.entity.UserDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun providesAppDatabase(@ApplicationContext context: Context): AppDatabase =
+        Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "MyDatabase"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Singleton
+    @Provides
+    fun providesUserDao(appDatabase: AppDatabase): UserDao = appDatabase.userDao()
+
+
+
+}
