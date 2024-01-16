9e752adc3007b65eec62378011ccba3050e7ec6e
Jorge1 <agullojorge@gmail.com>
2024-01-16 07:41:33 +0100

    Modified id field to UUID

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/data/entity/UserEntity.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/data/entity/UserEntity.kt
@@ -2,6 +2,7 @@ package com.softyorch.mvvmjetpackcompose.data.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.UUID
 
 @Entity(tableName = "user")
-data class UserEntity(@PrimaryKey val id: Int, val name: String, val age: Int)
+data class UserEntity(@PrimaryKey val id: UUID? = UUID.randomUUID(), val name: String, val age: Int)
