1ac9e94548995b9c779021efd75951d32676df2e
Jorge1 <agullojorge@gmail.com>
2024-01-16 19:30:24 +0100

    Implemented navigation

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.softyorch.mvvmjetpackcompose.ui.screen.users.MainScreen
+import com.softyorch.mvvmjetpackcompose.ui.navigation.NavigationManager
 import com.softyorch.mvvmjetpackcompose.ui.theme.MvvmJetpackComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -17,12 +17,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MvvmJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen()
+                    NavigationManager()
                 }
             }
         }
