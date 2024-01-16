716b98c9ca5048031ae7971504c32a08aabeedc0
Jorge1 <agullojorge@gmail.com>
2024-01-16 20:59:19 +0100

    Delete unused directives

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/splash/SplashScreen.kt
@@ -1,16 +1,13 @@
 package com.softyorch.mvvmjetpackcompose.ui.screen.splash
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.softyorch.mvvmjetpackcompose.R
 import com.softyorch.mvvmjetpackcompose.ui.navigation.NavigationRoutes
