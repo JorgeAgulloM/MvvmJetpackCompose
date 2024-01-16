c53a65cae27d6081ae40f3270e6748791876996d
Jorge1 <agullojorge@gmail.com>
2024-01-16 19:53:12 +0100

    Changed users to main route

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/main/UserUi.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/main/UserUi.kt
@@ -1,4 +1,4 @@
-package com.softyorch.mvvmjetpackcompose.ui.screen.users
+package com.softyorch.mvvmjetpackcompose.ui.screen.main
 
 import com.softyorch.mvvmjetpackcompose.domain.UserDomain
 import java.util.UUID
