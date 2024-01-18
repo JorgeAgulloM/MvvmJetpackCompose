2e0db2cc42c85494408f6ad58e24755ab43a8bec
Jorge1 <agullojorge@gmail.com>
2024-01-18 20:36:31 +0100

    Modified age to string on UI layer

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/models/UserUi.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/models/UserUi.kt
@@ -3,9 +3,9 @@ package com.softyorch.mvvmjetpackcompose.ui.models
 import com.softyorch.mvvmjetpackcompose.domain.UserDomain
 import java.util.UUID
 
-data class UserUi(val id: UUID, val name: String, val age: Int) {
+data class UserUi(val id: UUID, val name: String, val age: String) {
     companion object {
-        fun UserUi.toDomain(): UserDomain = UserDomain(id, name, age)
-        fun UserDomain.toUi(): UserUi = UserUi(id, name, age)
+        fun UserUi.toDomain(): UserDomain = UserDomain(id, name, age.toInt())
+        fun UserDomain.toUi(): UserUi = UserUi(id, name, age.toString())
     }
 }
