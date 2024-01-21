05e65342f2570a81d9480d2dfb845644d18b6229
Jorge1 <agullojorge@gmail.com>
2024-01-21 09:29:24 +0100

    [FIRST VERSION] created personalized TextField

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/componens/dataField/DataField.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/componens/dataField/DataField.kt
@@ -2,12 +2,15 @@ package com.softyorch.mvvmjetpackcompose.ui.componens.dataField
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -20,23 +23,28 @@ fun DataField(
     error: Boolean,
     onlyNumbs: Boolean = false,
     supportingText: String,
+    leadingIcon: ImageVector,
     onTextChange: (String) -> Unit
 ){
     val keyboardOptions = if (onlyNumbs) KeyboardOptions(keyboardType = KeyboardType.Number)
     else KeyboardOptions(capitalization = KeyboardCapitalization.Words)
+    val shape = MaterialTheme.shapes.small.copy(
+        bottomStart = ZeroCornerSize,
+        bottomEnd = ZeroCornerSize
+    )
 
-    OutlinedTextField(
+    TextField(
         value = text,
-        onValueChange = {
-            onTextChange(it)
-        },
-        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 2.dp),
+        onValueChange = { onTextChange(it) },
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp),
         enabled = enabled,
         label = { Text(text = label) },
-        placeholder = { Text(text = label) },
+        placeholder = { Text(text = label, color = MaterialTheme.colorScheme.surfaceVariant) },
+        leadingIcon = { Icon(leadingIcon, contentDescription = null) },
         supportingText = { if (error) Text(text = supportingText) },
         isError = error,
         keyboardOptions = keyboardOptions,
-        shape = MaterialTheme.shapes.large
+        shape = shape
     )
+
 }
