83f70657015bb393f5d70267c6ab6d484c7198e5
Jorge1 <agullojorge@gmail.com>
2024-01-22 14:08:11 +0100

    Added ImeActions

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/main/newUser/CreateUser.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/main/newUser/CreateUser.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarMonth
+import androidx.compose.material.icons.filled.ContactMail
+import androidx.compose.material.icons.filled.ContactPhone
+import androidx.compose.material.icons.filled.Mail
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +18,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -24,7 +29,8 @@ import com.softyorch.mvvmjetpackcompose.utils.EMPTY_STRING
 
 @Composable
 fun CreateUser(
-    viewModel: CreateUserViewModel = hiltViewModel<CreateUserViewModel>()
+    viewModel: CreateUserViewModel = hiltViewModel<CreateUserViewModel>(),
+    onCreateUser: () -> Unit
 ) {
 
     val user: UserUi by viewModel.user.collectAsStateWithLifecycle()
@@ -43,6 +49,7 @@ fun CreateUser(
             text = user.name,
             error = userErrors.name,
             supportingText = "Debe contener al menos 4 caracteres",
+            keyboardType = KeyboardType.Text,
             leadingIcon = Icons.Default.Person
         ) { name -> viewModel.onDataChange(user.copy(name = name)) }
         DataField(
@@ -50,6 +57,7 @@ fun CreateUser(
             text = user.surName ?: EMPTY_STRING,
             error = userErrors.surName,
             supportingText = "Debe contener al menos 4 caracteres",
+            keyboardType = KeyboardType.Text,
             leadingIcon = Icons.Default.Person
         ) { surName -> viewModel.onDataChange(user.copy(surName = surName)) }
         DataField(
@@ -57,28 +65,33 @@ fun CreateUser(
             text = user.phoneNumber,
             error = userErrors.phoneNumber,
             supportingText = "Solo puede contener número",
-            leadingIcon = Icons.Default.Person
+            keyboardType = KeyboardType.Phone,
+            leadingIcon = Icons.Default.Phone
         ) { phoneNumber -> viewModel.onDataChange(user.copy(phoneNumber = phoneNumber)) }
         DataField(
             label = "Email",
             text = user.email ?: EMPTY_STRING,
             error = userErrors.email,
             supportingText = "El email no es correcto",
-            leadingIcon = Icons.Default.Person
+            keyboardType = KeyboardType.Email,
+            leadingIcon = Icons.Default.Mail
         ) { email -> viewModel.onDataChange(user.copy(email = email)) }
         DataField(
             label = "Edad",
             text = user.age ?: EMPTY_STRING,
+            isLast = true,
             error = userErrors.age,
             supportingText = "Edad no puede estar vacío",
+            keyboardType = KeyboardType.Number,
             leadingIcon = Icons.Default.CalendarMonth
         ) { age -> viewModel.onDataChange(user.copy(age = age)) }
         Button(
             onClick = {
                 viewModel.setUsers()
                 focusManager.clearFocus()
+                onCreateUser()
             },
-            modifier = Modifier.padding(horizontal = 16.dp),
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
             enabled = stateError == StateError.Working
         ) {
             Text(text = "Crear usuario")
