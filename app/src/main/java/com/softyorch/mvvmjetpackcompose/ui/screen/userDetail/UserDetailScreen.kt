d225b8b5085021492cda2145cec99d1dd8c0bc02
Jorge1 <agullojorge@gmail.com>
2024-01-24 07:52:53 +0100

    Extract FakeTopBar

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/userDetail/UserDetailScreen.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/userDetail/UserDetailScreen.kt
@@ -1,22 +1,11 @@
 package com.softyorch.mvvmjetpackcompose.ui.screen.userDetail
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.softyorch.mvvmjetpackcompose.ui.screen.userDetail.details.UserDetails
 import java.util.UUID
 
@@ -27,34 +16,6 @@ fun UserDetailScreen(userId: UUID, onClick: () -> Unit) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        TopBar { onClick() }
-        UserDetails(userId = userId)
-    }
-}
-
-@Composable
-fun TopBar(
-    onBackClick: () -> Unit
-) {
-    Row(
-        modifier = Modifier.fillMaxWidth().padding(8.dp).background(
-            color = MaterialTheme.colorScheme.primaryContainer,
-            shape = MaterialTheme.shapes.large
-        ),
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        IconButton(
-            onClick = { onBackClick() }
-        ) {
-            Icon(Icons.Default.ArrowBack, contentDescription = null)
-        }
-        Row(
-            modifier = Modifier.fillMaxWidth(0.8f),
-            horizontalArrangement = Arrangement.SpaceAround,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(text = "Detalles")
-        }
+        UserDetails(userId = userId) { onClick() }
     }
 }
