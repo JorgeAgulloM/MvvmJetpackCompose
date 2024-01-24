beca266d79152a063c7028bf6cd3048d10235e15
Jorge1 <agullojorge@gmail.com>
2024-01-24 14:14:44 +0100

    Create Action SMS

---
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/core/intents/actions/ActionSMS.kt
@@ -0,0 +1,22 @@
+package com.softyorch.mvvmjetpackcompose.core.intents.actions
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+class ActionSMS {
+    fun send(context: Context ,phone: String, name: String) {
+        val intent = generateIntent(phone, name)
+        context.startActivity(intent)
+    }
+
+    private fun generateIntent(phone: String, name: String): Intent {
+        val action = Intent.ACTION_VIEW
+        val prefix = "smsto:"
+        val uri = Uri.parse("$prefix$phone")
+
+        return Intent(action, uri).apply {
+            putExtra("sms_body", "Hola $name")
+        }
+    }
+}
