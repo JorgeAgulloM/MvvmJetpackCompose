005f9333e026eb2c2386f068a5b263f9e1abb957
Jorge1 <agullojorge@gmail.com>
2024-01-25 23:13:42 +0100

    Delete unused implements

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/search/SearchScreen.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/search/SearchScreen.kt
@@ -1,6 +1,5 @@
 package com.softyorch.mvvmjetpackcompose.ui.screen.search
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
