83fd46085c3a98af74fd8ef85ebcb609f3c4a1fe
Jorge1 <agullojorge@gmail.com>
2024-01-25 08:14:41 +0100

    Change star icon. Removed null revision user.id

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/main/usersList/UsersList.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/main/usersList/UsersList.kt
@@ -3,12 +3,10 @@ package com.softyorch.mvvmjetpackcompose.ui.screen.main.usersList
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -26,7 +24,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -49,7 +46,7 @@ fun UsersList(
     val lazyState = rememberLazyListState()
 
     LazyColumn(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(0.95f),
         state = lazyState
     ) {
         when (val state = uiState) {
@@ -89,7 +86,7 @@ private fun User(user: UserUi, onClick: (String) -> Unit) {
 
     Row(
         modifier = Modifier.fillMaxWidth()
-            .clickable { user.id?.let { onClick(it.toString()) } }
+            .clickable { onClick(user.id.toString()) }
             .clip(shape = shape)
             .padding(start = 64.dp, end = 8.dp, top = 4.dp, bottom = 4.dp),
         horizontalArrangement = Arrangement.Start,
@@ -159,20 +156,11 @@ fun FavoriteOrBlocked(favorite: Boolean?, blocked: Boolean?) {
         tint = MaterialTheme.colorScheme.error
     )
 
-    if (favorite == true) Box(contentAlignment = Alignment.Center) {
-        Icon(
-            imageVector = Icons.Default.Star,
-            contentDescription = contentDescription,
-            modifier = Modifier.size(30.dp),
-            tint = MaterialTheme.colorScheme.onBackground
-        )
-        Icon(
-            imageVector = Icons.Default.Star,
-            contentDescription = contentDescription,
-            modifier = Modifier.size(24.dp),
-            tint = Color.Yellow.copy(alpha = 0.9f)
-        )
-    }
+    if (favorite == true) Icon(
+        imageVector = Icons.Default.Star,
+        contentDescription = contentDescription,
+        tint = MaterialTheme.colorScheme.primary
+    )
 }
 
 fun dateLongToString(timeInMillis: Long): String {
