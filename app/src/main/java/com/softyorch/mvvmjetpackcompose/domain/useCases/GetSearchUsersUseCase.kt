806f13f0f067bd34f6ccbf0e959e22f60ac9c52e
Jorge1 <agullojorge@gmail.com>
2024-01-26 09:34:44 +0100

    Improved search logic

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/domain/useCases/GetSearchUsersUseCase.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/domain/useCases/GetSearchUsersUseCase.kt
@@ -5,6 +5,7 @@ import com.softyorch.mvvmjetpackcompose.data.repository.IRepository
 import com.softyorch.mvvmjetpackcompose.domain.models.UserDomain
 import com.softyorch.mvvmjetpackcompose.domain.models.UserDomain.Companion.toDomain
 import com.softyorch.mvvmjetpackcompose.utils.EMPTY_STRING
+import com.softyorch.mvvmjetpackcompose.utils.deleteAccents
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
@@ -20,17 +21,29 @@ class GetSearchUsersUseCase @Inject constructor(private val repo: IRepository) {
                 list.filter { it.phoneNumber.startsWith(filter) }
             } else {
                 list.filter {
-                    val name = it.name.lowercase()
-                    val surName = it.surName?.lowercase() ?: EMPTY_STRING
-                    val completeName = "$name $surName"
-                    val email = it.email?.lowercase() ?: EMPTY_STRING
+                    val name = it.name.lowercase().deleteAccents()
+                    val processedSurName = it.surName?.let { value -> splitSurNames(value) }
+                    val surName = processedSurName?.first ?: EMPTY_STRING
+                    val lastName = processedSurName?.second ?: EMPTY_STRING
+                    val completeName = "$name $surName $lastName"
+                    val email = it.email?.lowercase()?.deleteAccents() ?: EMPTY_STRING
 
                     name.startsWith(filter) ||
-                    surName.startsWith(filter) ||
-                    completeName.startsWith(filter) ||
-                    email.startsWith(filter)
+                            surName.startsWith(filter) ||
+                            lastName.startsWith(filter) ||
+                            completeName.startsWith(filter) ||
+                            email.startsWith(filter)
                 }
             }
         }.map { listF -> listF.map { it.toDomain() } }
+
+    private fun splitSurNames(surNames: String): Pair<String, String> {
+        val surName = surNames.lowercase().deleteAccents()
+        return if (" " in surName) {
+            Pair(surName.split(" ")[0], surName.split(" ")[1])
+        } else {
+            Pair(surName, EMPTY_STRING)
+        }
+    }
 
 }
