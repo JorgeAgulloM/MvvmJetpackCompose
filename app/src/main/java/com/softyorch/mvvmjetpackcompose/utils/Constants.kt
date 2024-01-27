08a36949f9623de904931ef03793d47e7be689e6
Jorge1 <agullojorge@gmail.com>
2024-01-27 14:25:23 +0100

    Added colorList constant

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/utils/Constants.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/utils/Constants.kt
@@ -1,4 +1,10 @@
 package com.softyorch.mvvmjetpackcompose.utils
 
+import androidx.compose.ui.graphics.toArgb
+import com.softyorch.mvvmjetpackcompose.ui.theme.Pink80
+import com.softyorch.mvvmjetpackcompose.ui.theme.Purple80
+import com.softyorch.mvvmjetpackcompose.ui.theme.PurpleGrey80
+
 const val EMPTY_STRING = ""
 const val USER_ID = "userId"
+val colorList = listOf(Purple80.toArgb().toLong(), PurpleGrey80.toArgb().toLong(), Pink80.toArgb().toLong())
