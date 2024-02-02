7b6efdbc87544c333a6636f7813a07699c7a5271
Jorge1 <agullojorge@gmail.com>
2024-02-02 19:08:37 +0100

    Added function for validate only one field

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/models/errorValidator/UserValidatorImpl.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/models/errorValidator/UserValidatorImpl.kt
@@ -1,27 +1,80 @@
 package com.softyorch.mvvmjetpackcompose.ui.models.errorValidator
 
+import android.util.Log
 import android.util.Patterns
 import androidx.core.text.isDigitsOnly
+import com.softyorch.mvvmjetpackcompose.ui.models.UserErrorModel
+import com.softyorch.mvvmjetpackcompose.ui.models.UserUi
 import javax.inject.Inject
 
 class UserValidatorImpl @Inject constructor() : IUserValidator {
-    override fun isNameCorrect(name: String, size: Int): Boolean {
-        return name.length > size && !name.isDigitsOnly()
+
+    private val userError: UserErrorModel = UserErrorModel()
+
+    override fun searchError(user: UserUi, oldDataUser: UserUi): UserErrorModel {
+
+        val errorName = !isNameCorrect(user.name, 3)
+        val errorSurName = user.surName?.let {
+            if (it.isEmpty()) false else !isNameCorrect(it, 3)
+        } ?: false
+        val errorPhoneNumber = !isPhoneNumberCorrect(user.phoneNumber)
+        val errorEmail =
+            user.email?.let { if (it.isEmpty()) false else !isEmailCorrect(it) } ?: false
+        val errorAge =
+            user.age?.let { if (it.isEmpty()) false else !isAgeCorrect(it) } ?: false
+
+        return userError.copy(
+            name = errorName,
+            surName = errorSurName,
+            phoneNumber = errorPhoneNumber,
+            email = errorEmail,
+            age = errorAge
+        )
     }
 
-    override fun isPhoneNumberCorrect(phoneNumber: String): Boolean {
+    override fun searFieldError(user: UserUi, oldDataUser: UserUi): UserErrorModel {
+
+        if (user.name != oldDataUser.name)
+            return userError.copy(name = !isNameCorrect(user.name, 3))
+
+        if (user.surName != oldDataUser.surName)
+            return userError.copy(surName = user.surName?.let {
+                if (it.isEmpty()) false else !isNameCorrect(it, 3)
+            } ?: false)
+
+        if (user.phoneNumber != oldDataUser.phoneNumber)
+            return userError.copy(phoneNumber = !isPhoneNumberCorrect(user.phoneNumber))
+
+        if (user.email != oldDataUser.email)
+            return userError.copy(email = user.email?.let {
+                if (it.isEmpty()) false else !isEmailCorrect(it)
+            } ?: false)
+
+        if (user.age != oldDataUser.age)
+            return userError.copy(age = user.age?.let {
+                if (it.isEmpty()) false else !isAgeCorrect(it)
+            } ?: false)
+
+        return userError
+    }
+
+    private fun isNameCorrect(name: String, size: Int): Boolean {
+        return (name.length >= size && !name.isDigitsOnly())
+    }
+
+    private fun isPhoneNumberCorrect(phoneNumber: String): Boolean {
         return phoneNumber.isNotEmpty() && phoneNumber.isDigitsOnly()
     }
 
-    override fun isEmailCorrect(email: String): Boolean {
+    private fun isEmailCorrect(email: String): Boolean {
         return Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
-    override fun isAgeCorrect(age: String): Boolean {
+    private fun isAgeCorrect(age: String): Boolean {
         return try {
             if (age.isEmpty()) return false
             if (!age.isDigitsOnly()) return false
-            if (age[0].toString() == "0") return false
+            if (age[0].toString() <= "0") return false
             val intAge = age.toInt()
             intAge in 1..110
         } catch (e: Exception) {
