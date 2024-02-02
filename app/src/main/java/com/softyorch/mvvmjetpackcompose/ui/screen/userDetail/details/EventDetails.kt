4890faaa1763cd2ad0366b605cc204dd57809b83
Jorge1 <agullojorge@gmail.com>
2024-02-02 20:33:13 +0100

    A new event is implemented to enquire about the delete.

--- app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/userDetail/details/EventDetails.kt
+++ app/src/main/java/com/softyorch/mvvmjetpackcompose/ui/screen/userDetail/details/EventDetails.kt
@@ -3,5 +3,6 @@ package com.softyorch.mvvmjetpackcompose.ui.screen.userDetail.details
 sealed class EventDetails {
     data object Read: EventDetails()
     data object Edit: EventDetails()
+    data object Deleting: EventDetails()
     data object Delete: EventDetails()
 }
