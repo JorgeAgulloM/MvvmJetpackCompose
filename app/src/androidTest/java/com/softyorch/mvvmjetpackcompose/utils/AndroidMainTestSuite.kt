618170c01d3aeb4d63e5c5b9c1ac3b9f82c21b48
Jorge1 <agullojorge@gmail.com>
2024-02-05 07:56:36 +0100

    Create main test suite

---
+++ app/src/androidTest/java/com/softyorch/mvvmjetpackcompose/utils/AndroidMainTestSuite.kt
@@ -0,0 +1,11 @@
+package com.softyorch.mvvmjetpackcompose.utils
+
+import com.softyorch.mvvmjetpackcompose.data.entity.UserDaoTest
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    UserDaoTest::class
+)
+class AndroidMainTestSuite
