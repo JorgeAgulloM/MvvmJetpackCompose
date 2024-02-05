c9b772045cb090fcb46b33d2c917f44b162229f3
Jorge1 <agullojorge@gmail.com>
2024-02-05 07:28:39 +0100

    Create MainTestSuite

---
+++ app/src/test/java/com/softyorch/mvvmjetpackcompose/utils/MainTestSuite.kt
@@ -0,0 +1,23 @@
+package com.softyorch.mvvmjetpackcompose.utils
+
+import com.softyorch.mvvmjetpackcompose.data.repository.RepositoryImplTest
+import com.softyorch.mvvmjetpackcompose.domain.useCases.DeleteUserUseCaseTest
+import com.softyorch.mvvmjetpackcompose.domain.useCases.GetListUserUseCaseTest
+import com.softyorch.mvvmjetpackcompose.domain.useCases.GetSearchUsersUseCaseTest
+import com.softyorch.mvvmjetpackcompose.domain.useCases.GetUserUseCaseTest
+import com.softyorch.mvvmjetpackcompose.domain.useCases.SetUserUseCaseTest
+import com.softyorch.mvvmjetpackcompose.domain.useCases.UpdateUserUseCaseTest
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    DeleteUserUseCaseTest::class,
+    GetListUserUseCaseTest::class,
+    GetSearchUsersUseCaseTest::class,
+    GetUserUseCaseTest::class,
+    SetUserUseCaseTest::class,
+    UpdateUserUseCaseTest::class,
+    RepositoryImplTest::class
+)
+class MainTestSuite
