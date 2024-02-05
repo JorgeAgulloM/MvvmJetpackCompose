1892f8ff8528f10c30ba88a9b7cc2d4287d9647c
Jorge1 <agullojorge@gmail.com>
2024-02-05 07:55:54 +0100

    Implemented new tests

--- app/src/androidTest/java/com/softyorch/mvvmjetpackcompose/data/entity/UserDaoTest.kt
+++ app/src/androidTest/java/com/softyorch/mvvmjetpackcompose/data/entity/UserDaoTest.kt
@@ -5,14 +5,19 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.softyorch.mvvmjetpackcompose.data.AppDatabase
+import com.softyorch.mvvmjetpackcompose.utils.testContact
+import com.softyorch.mvvmjetpackcompose.utils.testContact2
+import com.softyorch.mvvmjetpackcompose.utils.userID
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertNull
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import java.util.UUID
 
 @RunWith(AndroidJUnit4::class)
 class UserDaoTest {
@@ -35,28 +40,53 @@ class UserDaoTest {
     }
 
     @Test
-    fun insertAndGetUser() = runBlocking {
-        val userID = UUID.randomUUID()
-        val testContact = UserEntity(
-            id = userID,
-            name = "Test",
-            surName = "User",
-            phoneNumber = "1234567890",
-            email = "test.user@example.com",
-            age = 30,
-            photoUri = null,
-            logo = null,
-            logoColor = null,
-            lastCall = null,
-            typeCall = null,
-            favorite = false,
-            phoneBlocked = false
-        )
+    fun testGetUsers() = runBlocking {
+        userDao.insertUsers(testContact)
+        userDao.insertUsers(testContact2)
+
+        val result = userDao.getUsers().first()
+
+        assertEquals(listOf(testContact, testContact2), result)
+    }
+
+    @Test
+    fun testGetUser() = runBlocking {
         userDao.insertUsers(testContact)
 
         val loaded = userDao.getUser(userID).first()
 
         assertEquals(testContact, loaded)
+    }
+
+    @Test
+    fun testInsertUser() = runBlocking {
+        userDao.insertUsers(testContact)
+
+        val loaded = userDao.getUser(userID).first()
+
+        assertEquals(testContact, loaded)
+    }
+
+    @Test
+    fun testUpdateUser() = runBlocking {
+        userDao.insertUsers(testContact)
+
+        val updateUSer = testContact.copy(name = "Jorge")
+        userDao.updateUser(updateUSer)
+
+        val loaded = userDao.getUser(userID).first()
+
+        assertEquals(updateUSer, loaded)
+    }
+
+    @Test
+    fun testDeleteUser() = runBlocking {
+        userDao.insertUsers(testContact)
+        userDao.deleteUser(testContact)
+
+        val result = userDao.getUser(userID).firstOrNull()
+
+        assertNull(result)
     }
 
 }
