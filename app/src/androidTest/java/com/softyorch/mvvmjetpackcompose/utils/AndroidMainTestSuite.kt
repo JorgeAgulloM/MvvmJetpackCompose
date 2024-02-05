d1bf487a8b7bc723f86a0047950aa0a27f1b0e41
Jorge1 <agullojorge@gmail.com>
2024-02-05 21:08:53 +0100

    Added new test in AndroidMainTestSuite

--- app/src/androidTest/java/com/softyorch/mvvmjetpackcompose/utils/AndroidMainTestSuite.kt
+++ app/src/androidTest/java/com/softyorch/mvvmjetpackcompose/utils/AndroidMainTestSuite.kt
@@ -1,11 +1,18 @@
 package com.softyorch.mvvmjetpackcompose.utils
 
+import com.softyorch.mvvmjetpackcompose.core.intents.actions.ActionDialTest
+import com.softyorch.mvvmjetpackcompose.core.intents.actions.ActionEmail
+import com.softyorch.mvvmjetpackcompose.core.intents.actions.ActionEmailTest
+import com.softyorch.mvvmjetpackcompose.core.intents.actions.ActionSMSTest
 import com.softyorch.mvvmjetpackcompose.data.entity.UserDaoTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    UserDaoTest::class
+    UserDaoTest::class,
+    ActionDialTest::class,
+    ActionEmailTest::class,
+    ActionSMSTest::class
 )
 class AndroidMainTestSuite
