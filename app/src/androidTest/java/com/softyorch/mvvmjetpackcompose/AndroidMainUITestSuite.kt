07c9043c2f5df84463804e462f6bd726636b9966
Jorge1 <agullojorge@gmail.com>
2024-02-09 15:25:43 +0100

    Create Suite for UI test. Implemented DataFieldTest

---
+++ app/src/androidTest/java/com/softyorch/mvvmjetpackcompose/AndroidMainUITestSuite.kt
@@ -0,0 +1,11 @@
+package com.softyorch.mvvmjetpackcompose
+
+import com.softyorch.mvvmjetpackcompose.ui.componens.DataFieldTest
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    DataFieldTest::class
+)
+class AndroidMainUITestSuite
