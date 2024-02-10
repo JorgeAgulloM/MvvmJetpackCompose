2c46433bc6ebcd55e277d2ee025ba08b16dc240d
Jorge1 <agullojorge@gmail.com>
2024-02-10 08:46:23 +0100

    Implemented integration test

--- app/src/androidTest/java/com/softyorch/mvvmjetpackcompose/AndroidMainTestSuite.kt
+++ app/src/androidTest/java/com/softyorch/mvvmjetpackcompose/AndroidMainTestSuite.kt
@@ -4,6 +4,9 @@ import com.softyorch.mvvmjetpackcompose.core.intents.actions.ActionDialTest
 import com.softyorch.mvvmjetpackcompose.core.intents.actions.ActionEmailTest
 import com.softyorch.mvvmjetpackcompose.core.intents.actions.ActionSMSTest
 import com.softyorch.mvvmjetpackcompose.data.entity.UserDaoTest
+import com.softyorch.mvvmjetpackcompose.integrationTests.DetailsUserCrudIntegrationTest
+import com.softyorch.mvvmjetpackcompose.integrationTests.GetUsersIntegrationTest
+import com.softyorch.mvvmjetpackcompose.integrationTests.SetUserIntegrationTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
@@ -12,6 +15,9 @@ import org.junit.runners.Suite
     UserDaoTest::class,
     ActionDialTest::class,
     ActionEmailTest::class,
-    ActionSMSTest::class
+    ActionSMSTest::class,
+    GetUsersIntegrationTest::class,
+    SetUserIntegrationTest::class,
+    DetailsUserCrudIntegrationTest::class
 )
 class AndroidMainTestSuite
