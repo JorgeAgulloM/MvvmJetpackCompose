975aa8849e80bdb6e0ed63e2c868d20b4dafbba1
Jorge1 <agullojorge@gmail.com>
2024-02-10 06:46:36 +0100

    Implemented ImageUserAutoTest

--- app/src/androidTest/java/com/softyorch/mvvmjetpackcompose/AndroidMainUITestSuite.kt
+++ app/src/androidTest/java/com/softyorch/mvvmjetpackcompose/AndroidMainUITestSuite.kt
@@ -1,11 +1,13 @@
 package com.softyorch.mvvmjetpackcompose
 
 import com.softyorch.mvvmjetpackcompose.ui.componens.DataFieldTest
+import com.softyorch.mvvmjetpackcompose.ui.componens.ImageUserAutoTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    DataFieldTest::class
+    DataFieldTest::class,
+    ImageUserAutoTest::class
 )
 class AndroidMainUITestSuite
