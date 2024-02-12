f02d6bc89367eb974899a2277545682f6f563122
Jorge1 <agullojorge@gmail.com>
2024-02-12 17:38:25 +0100

    Update material-icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,9 +78,8 @@ dependencies {
     implementation("androidx.work:work-runtime-ktx:2.9.0")
 
     /** material icons */
-    implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation("androidx.compose.material:material-icons-extended:1.6.1")
 
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.0")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
